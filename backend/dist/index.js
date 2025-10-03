c1d7632ecb1a88e376f9ec4e93b8a0af336e1255
ddas4548 <ddas4548@gmail.com>
2025-10-03 11:57:31 +0530

    project script set up

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get('/ping', (res) => {
-    res.send("<h1>Hello from your Express server!</h1>");
+app.get("/ping", (_req, res) => {
+    res.json({
+        message: "pong",
+    });
 });
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log("Server is running on port 3000");
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,13 @@
-import express, {Express, Request, Response } from 'express';
-
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 
-app.get('/ping', (_req: Request,res:Response) => {
-   res.json({
-    message: "pong"
-   });
-})
+app.get("/ping", (_req: Request, res: Response) => {
+  res.json({
+    message: "pong",
+  });
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
