3625d0636a531f09e27a9d0384de2bf5f14af2d9
ddas4548 <ddas4548@gmail.com>
2025-10-04 11:51:56 +0530

    video controller set up

---
+++ backend/src/controllers/video.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+export const uploadVideoController = async (req: Request, res: Response) => {
+     if(!req.file) {
+        res.status(400).json({
+            success: false,
+            message: 'No file uploaded'
+        });
+        return;
+    }
+
+    const videoPath = req.file.path;
+
+     res.status(200).json({
+        success: true,
+        message: 'Video uploaded successfully',
+        videoPath
+    });
+
+}
