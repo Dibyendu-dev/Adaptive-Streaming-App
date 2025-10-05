1243cccf23515f66534c9bf896d77950606b89c4
ddas4548 <ddas4548@gmail.com>
2025-10-05 12:27:46 +0530

    streaming added in frontend

--- backend/dist/services/video.service.js
+++ backend/dist/services/video.service.js
@@ -1,20 +1,12 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.processVideoForHLS = void 0;
-const promises_1 = __importDefault(require("fs/promises"));
-const resolution = [
+const fs_1 = __importDefault(require("fs"));
+const fluent_ffmpeg_1 = __importDefault(require("fluent-ffmpeg"));
+const resolutions = [
     { width: 1920, height: 1080, bitRate: 2000 }, // 1080p
     { width: 1280, height: 720, bitRate: 1000 }, // 720p
     { width: 854, height: 480, bitRate: 500 }, // 480p
@@ -23,7 +15,44 @@ const resolution = [
 const processVideoForHLS = (inputPath, //The path to the input video file.
 outputPath, //The path where the processed HLS files will be saved
 callback //A callback function that is called when the processing is complete.The callback receives an error object if an error occurred, and the master playlist string if the processing was successful.
-) => __awaiter(void 0, void 0, void 0, function* () {
-    yield promises_1.default.mkdir(outputPath, { recursive: true });
-});
+) => {
+    fs_1.default.mkdirSync(outputPath, { recursive: true });
+    const masterPlaylist = `${outputPath}/master.m3u8`;
+    const masterContent = [];
+    let countProcessing = 0;
+    resolutions.forEach((resolution) => {
+        const variantOutput = `${outputPath}/${resolution.height}p`;
+        const varientPlaylist = `${variantOutput}/playlist.m3u8`;
+        fs_1.default.mkdirSync(variantOutput, { recursive: true });
+        (0, fluent_ffmpeg_1.default)(inputPath)
+            .outputOption([
+            `-vf scale=w=${resolution.width}:h=${resolution.height}`,
+            `-b:v ${resolution.bitRate}k`,
+            "-codec:v libx264",
+            "-codec:a aac",
+            "-hls_time 10",
+            "-hls_playlist_type vod",
+            `-hls_segment_filename ${variantOutput}/segment%03d.ts`,
+        ])
+            .output(varientPlaylist)
+            .on("end", () => {
+            // When the processing ends for a resolution, add the variant playlist to the master playlist
+            masterContent.push(`#EXT-X-STREAM-INF:BANDWIDTH=${resolution.bitRate * 1000},RESOLUTION=${resolution.width}x${resolution.height}\n${resolution.height}p/playlist.m3u8`);
+            countProcessing += 1;
+            if (countProcessing === resolutions.length) {
+                console.log("Processing complete");
+                console.log(masterContent);
+                // When the processing ends for all resolutions, create the master playlist
+                fs_1.default.writeFileSync(masterPlaylist, `#EXTM3U\n${masterContent.join("\n")}`);
+                // place where video processing ends
+                callback(null, masterPlaylist); // Call the callback with the master playlist path
+            }
+        })
+            .on("error", (error) => {
+            console.log("An error occurred:", error);
+            callback(error); // Call the callback with the error
+        })
+            .run();
+    });
+};
 exports.processVideoForHLS = processVideoForHLS;
--- backend/src/controllers/video.controller.ts
+++ backend/src/controllers/video.controller.ts
@@ -1,4 +1,6 @@
+import fs from 'fs';
 import { Request, Response } from 'express';
+import { processVideoForHLS } from '../services/video.service';
 
 export const uploadVideoController = async (req: Request, res: Response) => {
      if(!req.file) {
@@ -10,11 +12,29 @@ export const uploadVideoController = async (req: Request, res: Response) => {
     }
 
     const videoPath = req.file.path;
+    const outputPath = `output/${Date.now()}`
+
+    processVideoForHLS(videoPath, outputPath,(err, _) =>{
+         if(err) {
+            res.status(500).json({
+                success: false,
+                message: 'An error occurred while processing the video'
+            });
+            return;
+        }
+
+        // Delete the video file after processing
+        fs.unlink(videoPath, (err) => {
+            if(err) {
+                console.log('An error occurred while deleting the video file:', err);
+            }
+        });
+    })
 
      res.status(200).json({
         success: true,
         message: 'Video uploaded successfully',
-        videoPath
+       
     });
 
 }
