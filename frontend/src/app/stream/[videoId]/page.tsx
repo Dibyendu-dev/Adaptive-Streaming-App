eee5a3e1253aa180103ab5929b28c2f345d6fc14
ddas4548 <ddas4548@gmail.com>
2025-10-05 20:27:02 +0530

    streaming works.

--- frontend/src/app/stream/[videoId]/page.tsx
+++ frontend/src/app/stream/[videoId]/page.tsx
@@ -1,32 +1,33 @@
-'use client'
+"use client";
 import { useParams } from "next/navigation";
 import { useEffect, useRef } from "react";
 import HLS from "hls.js";
 
 export default function Page() {
-    
-    const params = useParams<{ videoId: string }>()
-    const videoId = params.videoId;
+  const params = useParams<{ videoId: string }>();
+  const videoId = params.videoId;
 
-    const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-     useEffect(()=>{
-        if(videoId && HLS.isSupported() ){
-            const hls = new HLS();
-            hls.loadSource(`http://localhost:3000/output/${videoId}/master.m3u8`)
-            hls.attachMedia(videoRef.current!)
-        }
-     },[videoId])
+  useEffect(() => {
+    if (videoId && HLS.isSupported()) {
+      const hls = new HLS();
+      hls.loadSource(`http://localhost:3000/output/${videoId}/master.m3u8`);
+      hls.attachMedia(videoRef.current!);
+    }
+  }, [videoId]);
 
-   return (
+  return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center py-8 px-4">
-      <div className=" w-full bg-white shadow-lg rounded-lg p-6">
+      <div className=" max-w-md w-full bg-white shadow-lg rounded-lg p-6">
         <div className="mt-4">
-          <h2 className="text-lg font-semibold text-gray-900 mb-2">video preview { videoId}</h2>
+          <h2 className="text-lg font-semibold text-gray-900 mb-2">
+            video preview {videoId}
+          </h2>
           <video
-            controls 
+            controls
             className="rounded-lg border border-gray-300"
-            width={'100%'}
+            width={"100%"}
             ref={videoRef}
           />
         </div>
